31782e37bc86be2cfa514f47fa1f21cfc17ebfa3
ddargoti2348 <ddargoti.2348@unicesmag.edu.co>
2026-03-05 04:14:51 +0000

    Revision

--- soluciones/04.py
+++ soluciones/04.py
@@ -9,3 +9,9 @@ datos['nombre'] = datos ['nombre_cifrado'].apply(decifrar_nombre)
 
 Frecuencias = datos['nombre'].value_counts()
 
+nombre_frecuente = Frecuencias.idxmax()
+Numero_de_veces = Frecuencias.max()
+
+print ("El nombre mas frecuente es:", nombre_frecuente)
+print ("Aparece:",Numero_de_veces, "veces")
+
